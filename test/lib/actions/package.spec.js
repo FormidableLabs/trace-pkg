36ab491ccdda41fc79bd44b5b3b724729c50773f
Ryan Roemer <ryan.roemer@formidable.com>
2020-11-02 16:01:54 -0800

    Start a config file

--- test/lib/actions/package.spec.js
+++ test/lib/actions/package.spec.js
@@ -1,5 +1,8 @@
 "use strict";
 
 describe("lib/actions/package", () => {
+  it("packages across multiple different working directories"); // TODO
+  it("outputs zip files to multiple different locations"); // TODO
+
   it("TODO: IMPLEMENT SUITE"); // TODO
 });
