b34f2439f6e1ce5ca37a84f88bbed1a682bb329d
Ryan Roemer <ryan.roemer@formidable.com>
2020-11-04 14:38:54 -0800

    Start adding real tests.

--- test/lib/actions/package.spec.js
+++ test/lib/actions/package.spec.js
@@ -2,8 +2,14 @@
 
 const mock = require("mock-fs");
 const sinon = require("sinon");
+const AdmZip = require("adm-zip");
 
 const createPackage = require("../../../lib/actions/package").package;
+
+const zipContents = (zipPath) => {
+  const zip = new AdmZip(zipPath);
+  return zip.getEntries().map(({ entryName }) => entryName);
+};
 
 describe("lib/actions/package", () => {
   let sandbox;
@@ -59,7 +65,71 @@ describe("lib/actions/package", () => {
   // TODO
   // - [ ] `options.cwd`
   // - [ ] `packages.KEY.cwd`
-  it("packages across multiple different working directories"); // TODO
+  it("packages across multiple different working directories", async () => {
+    mock({
+      src: {
+        "one.js": "module.exports = require('./one/dep');",
+        one: {
+          "dep.js": "module.exports = require('./deeper/dep');",
+          deeper: {
+            "dep.js": "module.exports = \"deeper-dep\";"
+          }
+        },
+        "two.json": "{ \"msg\": \"two\" }",
+        "two.css": "body.two { background-color: pink; }",
+        three: {
+          "index.js": "module.exports = \"three\";"
+        }
+      }
+    });
+
+    await createPackage({
+      opts: {
+        config: {
+          options: {
+            cwd: "src"
+          },
+          packages: {
+            "../../.build/one": {
+              cwd: "src/one",
+              trace: [
+                "../one.js"
+              ]
+            },
+            two: {
+              output: "../.build/two.zip",
+              include: [
+                "two.*"
+              ]
+            },
+            three: {
+              cwd: "src/three",
+              output: "../../.build/three.zip",
+              trace: [
+                "index.js"
+              ],
+              include: [
+                "index.js" // Double include (should only have one file in zip)
+              ]
+            }
+          }
+        }
+      }
+    });
+
+    expect(zipContents(".build/one.zip")).to.eql([
+      "one.js",
+      "deeper/dep.js",
+      "dep.js"
+    ]);
+    expect(zipContents(".build/two.zip")).to.eql([
+      "two.css",
+      "two.json"
+    ]);
+    expect(zipContents(".build/three.zip")).to.eql([
+      "index.js"
+    ]);
+  });
 
   // TODO
   // - [ ] `KEY`
