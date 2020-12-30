ce3f609f7da252a620d0b99b784891db14a60810
sawyer burnett <sawyerburnett@gmail.com>
2020-12-29 18:55:43 -0700

    improve testing
    convert to use sandbox and remove unnecessary variables

--- test/lib/log.spec.js
+++ test/lib/log.spec.js
@@ -3,49 +3,49 @@
 /* eslint-disable no-console */
 
 const { setLoggingOptions, error, log, warn } = require("../../lib/log");
-const chai = require("chai");
 const sinon = require("sinon");
-const sinonChai = require("sinon-chai");
-chai.use(sinonChai);
 
 describe("lib/log", () => {
+  let sandbox;
+  let logStub;
+  let errorStub;
   beforeEach(() => {
-    sinon.spy(console, "log");
-    sinon.spy(console, "error");
+    sandbox = sinon.createSandbox();
+    logStub = sandbox.stub(console, "log");
+    errorStub = sandbox.stub(console, "error");
   });
 
   afterEach(() => {
-    console.log.restore();
-    console.error.restore();
+    sandbox.restore();
   });
 
   describe("default options should log", () => {
     it("log should call console.log", () => {
       log();
-      expect(console.log).to.be.called;
+      expect(logStub).to.be.called;
     });
     it("warn should call console.log", () => {
       warn();
-      expect(console.log).to.be.called;
+      expect(logStub).to.be.called;
     });
     it("error should call console.error", () => {
       error();
-      expect(console.error).to.be.called;
+      expect(errorStub).to.be.called;
     });
   });
   describe("silent mode shouldn't log", () => {
     it("log shouldn't call console.log", () => {
       setLoggingOptions({ silent: true });
       log();
-      expect(console.log).to.not.be.called;
+      expect(logStub).to.not.be.called;
     });
     it("warn shouldn't call console.log", () => {
       warn();
-      expect(console.log).to.not.be.called;
+      expect(logStub).to.not.be.called;
     });
     it("error shouldn't call console.error", () => {
       error();
-      expect(console.error).to.not.be.called;
+      expect(logStub).to.not.be.called;
     });
   });
 })
